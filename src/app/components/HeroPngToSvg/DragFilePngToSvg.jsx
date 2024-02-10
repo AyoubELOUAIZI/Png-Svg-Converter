b294252535be9c4c22dccfe7496a6de70eb3d568
ELOUAIZI Ayoub <ayoubelouaizi@gmail.com>
2024-02-10 22:01:25 +0100

    png to svg clean code

--- src/app/components/HeroPngToSvg/DragFilePngToSvg.jsx
+++ src/app/components/HeroPngToSvg/DragFilePngToSvg.jsx
@@ -10,7 +10,6 @@ const DragFilePngToSvg = forwardRef((props, ref) => {
   const iconUrl =
     "https://res.cloudinary.com/dm9udoven/image/upload/v1707164619/svgtopng%20Site/image-upload_g8vfcr.svg";
   const [etrState, setEtrState] = useState(null);
-  const [svgDataUrl, setSvgDataUrl] = useState(""); // State to hold the PNG data URL
   const [svg, setSvg] = useState(null);
 
   useEffect(() => {
@@ -72,7 +71,7 @@ const DragFilePngToSvg = forwardRef((props, ref) => {
 
       var preview = document.getElementById("preview");
       preview.src = e.target.result;
-      console.log("🚀 ~ displayPreview ~ e.target.result:", e.target.result);
+      // console.log("🚀 ~ displayPreview ~ e.target.result:", e.target.result);
       // setEtrState(e.target.result);
       setEtrState(file);
     };
@@ -80,80 +79,79 @@ const DragFilePngToSvg = forwardRef((props, ref) => {
   }
 
   const convertPngToSvg = (event) => {
-
-        if (!etrState) {
-      console.error("No image selected for conversion.");
-      return;
-    }
-
     // const file = event.target.files[0];
     if (etrState) {
-        convertPngToSvg2(etrState).then(svgString => {
-            setSvg(svgString);
-        }).catch(error => {
-            console.error('Error converting PNG to SVG:', error);
+      convertPngToSvg2(etrState)
+        .then((svgString) => {
+          setSvg(svgString);
+        })
+        .catch((error) => {
+          console.error("Error converting PNG to SVG:", error);
         });
+    } else {
+      // console.error("No image selected for conversion.");
+      alert("No image selected for conversion.");
     }
-};
-  
-const convertPngToSvg2 = (file) => {
-  console.log("file",file)
+  };
 
-  return new Promise((resolve, reject) => {
+  const convertPngToSvg2 = (file) => {
+    // console.log("file", file);
+
+    return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.onload = (e) => {
-          const img = new Image();
-          img.onload = () => {
-              const canvas = document.createElement('canvas');
-              const ctx = canvas.getContext('2d');
-              canvas.width = img.width;
-              canvas.height = img.height;
-              ctx.drawImage(img, 0, 0);
-              const imageData = ctx.getImageData(0, 0, img.width, img.height).data;
-              
-              let svgPaths = '';
-              for (let y = 0; y < img.height; y++) {
-                  for (let x = 0; x < img.width; x++) {
-                      const pixelIndex = (y * img.width + x) * 4;
-                      const r = imageData[pixelIndex];
-                      const g = imageData[pixelIndex + 1];
-                      const b = imageData[pixelIndex + 2];
-                      const a = imageData[pixelIndex + 3];
-                      
-                      if (a > 0) { // Skip transparent pixels
-                          svgPaths += `<path fill="rgb(${r},${g},${b})" d="M${x} ${y}h1v1h-1z"/>`;
-                      }
-                  }
+        const img = new Image();
+        img.onload = () => {
+          const canvas = document.createElement("canvas");
+          const ctx = canvas.getContext("2d");
+          canvas.width = img.width;
+          canvas.height = img.height;
+          ctx.drawImage(img, 0, 0);
+          const imageData = ctx.getImageData(0, 0, img.width, img.height).data;
+
+          let svgPaths = "";
+          for (let y = 0; y < img.height; y++) {
+            for (let x = 0; x < img.width; x++) {
+              const pixelIndex = (y * img.width + x) * 4;
+              const r = imageData[pixelIndex];
+              const g = imageData[pixelIndex + 1];
+              const b = imageData[pixelIndex + 2];
+              const a = imageData[pixelIndex + 3];
+
+              if (a > 0) {
+                // Skip transparent pixels
+                svgPaths += `<path fill="rgb(${r},${g},${b})" d="M${x} ${y}h1v1h-1z"/>`;
               }
-              
-              const svgString = `<svg xmlns="http://www.w3.org/2000/svg" width="${img.width}" height="${img.height}">${svgPaths}</svg>`;
-              resolve(svgString);
-          };
-          img.src = e.target.result;
+            }
+          }
+
+          const svgString = `<svg xmlns="http://www.w3.org/2000/svg" width="${img.width}" height="${img.height}">${svgPaths}</svg>`;
+          resolve(svgString);
+        };
+        img.src = e.target.result;
       };
       reader.readAsDataURL(file);
-  });
-};
+    });
+  };
 
   const handleDownloadSvgImage = () => {
     if (svg) {
-        downloadSvg(svg, 'image.svg');
+      downloadSvg(svg, "image.svg");
     }
-};
+  };
 
-const downloadSvg = (svgString, fileName) => {
-  const blob = new Blob([svgString], { type: 'image/svg+xml' });
-  const url = URL.createObjectURL(blob);
-  const link = document.createElement('a');
-  link.href = url;
-  link.download = fileName;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-  URL.revokeObjectURL(url);
-};
+  const downloadSvg = (svgString, fileName) => {
+    const blob = new Blob([svgString], { type: "image/svg+xml" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  };
 
-  
   return (
     <div
       className="md:w-[400px] relative border-2  border-pink-300 border-dashed rounded-lg p-6"
--- src/app/pngtosvg/page.jsx
+++ src/app/pngtosvg/page.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Hero3 from '../components/Hero3/Hero3'
 import HeroPngToSvg from '../components/HeroPngToSvg/HeroPngToSvg'
-import YourComponent from '../components/HeroPngToSvg/YourComponent';
 const imgUrl1="https://res.cloudinary.com/dm9udoven/image/upload/v1707227422/svgtopng%20Site/pngtosvg_uvyfak.webp";
 const imgUrl2="https://res.cloudinary.com/dm9udoven/image/upload/v1707226167/svgtopng%20Site/svgtopng2_u8vjww.webp";
 
@@ -10,7 +9,6 @@ const page = () => {
         <div>
         <div className='flex justify-center items-center'>
             <HeroPngToSvg/>
-            <YourComponent/>
         </div>
             <Hero3 imgUrl={imgUrl1}/>
             <Hero3 imgUrl={imgUrl2}/>
